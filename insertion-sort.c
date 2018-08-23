a184573c32742fda016e0e958ff1fc9b8276307e
LAVID <lavid@MacBook-Pro-de-LAVID.local>
2018-08-23 09:48:07 -0300

    insertion and selection sort implementation

---
+++ insertion-sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    FILE *f;
+    char c;
+    f=fopen("instancias-num/couting.txt","r");
+    int instancias[5000];
+    int n = 100, pivo=0,j=0;
+    for (int i = 0; i < n; i++){
+        fscanf(f, "%d", &instancias[i]);
+    }
+    for(int i=0;i<n;i++){
+        pivo = instancias[i];
+        j = i-1;
+        while(j>=0 && instancias[j]>pivo){
+            instancias[j+1] = instancias[j];
+            j=j-1;
+        }
+        instancias[j+1] = pivo;
+    }
+    for(int i=0;i<n;i++)
+        printf("%d\n", instancias[i]);
+    fclose(f);
+    return 0;
+}
---
+++ selection-sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    FILE *f;
+    char c;
+    f=fopen("instancias-num/couting.txt","r");
+    int instancias[5000];
+    int n = 100;
+    int i_min,temp;
+
+    for (int i = 0; i < n; i++){
+        fscanf(f, "%d", &instancias[i]);
+    }
+    for (int i = 0; i < (n-1); i++) 
+    {
+        i_min = i;
+        for (int j = (i+1); j < n; j++) {
+        if(instancias[j] < instancias[i_min]) 
+            i_min = j;
+        }
+        if (instancias[i] != instancias[i_min]) {
+            temp = instancias[i];
+            instancias[i] = instancias[i_min];
+            instancias[i_min] = temp;
+        }
+    }
+    for(int i=0;i<n;i++)
+        printf("%d\n", instancias[i]);
+    fclose(f);
+    return 0;
+}
