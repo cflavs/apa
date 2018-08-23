a2eaee0e919e09d14027be6bafeffb58a799cd03
LAVID <lavid@MacBook-Pro-de-LAVID.local>
2018-08-23 10:33:50 -0300

    completed: adding some comments in the program

--- Atividade 1 - Insertion e Selection Sort/insertion-sort.c
+++ Atividade 1 - Insertion e Selection Sort/insertion-sort.c
@@ -4,22 +4,24 @@
 int main(int argc, char* argv[]) {
     FILE *f;
     char *filename;
+    int instancias[5000];
+    int n = 100, pivo=0,j=0;
+    
+    //Verifica se o usuário incluiu o nome do arquivo como parametro do programa
     if(argc != 2)
     {
-        printf( "Missing filename");
-        return 0; //should be two arguments, exe name & file path/name
+        printf("Falta o nome do arquivo a ser ordenado como parametro do programa");
+        return 0; 
     }
     else{
         filename=argv[1];
     }
+    //Leitura do arquivo
     f=fopen(filename,"r");
-
-    int instancias[5000];
-    int n = 100, pivo=0,j=0;
-
     for (int i = 0; i < n; i++){
         fscanf(f, "%d", &instancias[i]);
     }
+    //Insertion-sort
     for(int i=0;i<n;i++){
         pivo = instancias[i];
         j = i-1;
@@ -29,6 +31,7 @@ int main(int argc, char* argv[]) {
         }
         instancias[j+1] = pivo;
     }
+    //Exibe instancias ordenadas
     for(int i=0;i<n;i++)
         printf("%d\n", instancias[i]);
     fclose(f);
--- Atividade 1 - Insertion e Selection Sort/selection-sort.c
+++ Atividade 1 - Insertion e Selection Sort/selection-sort.c
@@ -4,26 +4,28 @@
 int main(int argc, char* argv[]) {
     FILE *f;
     char *filename;
-    if(argc != 2)
-    {
-        printf( "Missing filename");
-        return 0; //should be two arguments, exe name & file path/name
-    }
-    else{
-        filename=argv[1];
-    }
-    f=fopen(filename,"r");
-
     int instancias[5000];
     int n = 100;
     int i_min,temp;
 
+    //Verifica se o usuário incluiu o nome do arquivo como parametro do programa
+    if(argc != 2)
+    {
+        printf("Falta o nome do arquivo a ser ordenado como parametro do programa");
+        return 0;
+    }
+    else{
+        filename=argv[1];
+    }
+    //Leitura do arquivo
+    f=fopen(filename,"r");
     for (int i = 0; i < n; i++){
         fscanf(f, "%d", &instancias[i]);
     }
+    //Selection-sort
     for (int i = 0; i < (n-1); i++) 
     {
-        i_min = i;
+        i_min = i; 
         for (int j = (i+1); j < n; j++) {
         if(instancias[j] < instancias[i_min]) 
             i_min = j;
@@ -34,6 +36,7 @@ int main(int argc, char* argv[]) {
             instancias[i_min] = temp;
         }
     }
+    //Exibe instancias ordenadas
     for(int i=0;i<n;i++)
         printf("%d\n", instancias[i]);
     fclose(f);
